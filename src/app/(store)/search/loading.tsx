239ef7e9f290b3029f4b9b11e0e1afac6e12cb50
liajux <julialbritto@gmail.com>
2024-01-06 19:29:39 -0800

    ⚠️ Fix: static-boundaries

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 
+import { CurrentSearch } from './current-search'
 import { Skeleton } from '@/components/skeleton'
 
 export default function SearchLoading() {
@@ -11,9 +13,9 @@ export default function SearchLoading() {
 
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[480px]" />
